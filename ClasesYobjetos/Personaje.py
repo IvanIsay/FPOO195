d8a14b790ac4aec420072b636ae8904e36b51a66
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-02-23 17:07:53 -0600

    Clase Spartan

--- ClasesYobjetos/Personaje.py
+++ ClasesYobjetos/Personaje.py
@@ -22,7 +22,7 @@ class Personaje:
         print("Arma recargada al "+ str(cargador)+ "%")
       
       
- #creamos el objeto Spartan de la clase personaje       
+#creamos el objeto Spartan de la clase personaje       
 Spartan = Personaje()
 
 #Usamos los atributos Spartan
