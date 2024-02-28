18be946b02b7945398c10852a6094c14d0b32b00
lOkY <102552151+IvanIsay@users.noreply.github.com>
2024-02-28 17:13:04 -0600

    Encapsulamos la clase personaje

--- ClasesYobjetos/Personaje.py
+++ ClasesYobjetos/Personaje.py
@@ -1,20 +1,42 @@
 class Personaje:
     
     #atibuto de personaje
-    # Declaramos el contructor para crear los objetos
+    #Declaramos el contructor para crear los objetos
+    #cambiamos a privados los atributos del personaje
     def __init__(self,esp,nom,alt):
-        self.especie= esp
-        self.nombre= nom
-        self.altura= alt
+        self.__especie= esp
+        self.__nombre= nom
+        self.__altura= alt
     
     #Metodos del personaje
     def correr(self,estado):
         if(estado):
-            print("el personaje "+ self.nombre +" esta corriendo")
+            print("el personaje "+ self.__nombre +" esta corriendo")
         else:
-            print("el personaje "+ self.nombre +" esta muerto")   
+            print("el personaje "+ self.__nombre +" esta muerto")   
             
             
     def lanzarGranada(self):
-        print(self.nombre+" Pego una granada") 
+        print(self.__nombre+" Pego una granada") 
+        
+    
+    
+	# Definimos Getters y Setters para acceder a los atributos
+    def getEspecie(self):
+        return self.__especie
+      
+    def setEspecie(self, ex):
+        self.__especie = ex
+
+    def getNombre(self):
+        return self.__nombre
+      
+    def setNombre(self, nx):
+        self.__nombre = nx
+        
+    def getAltura(self):
+        return self.__altura
+      
+    def setAltura(self, ax):
+        self.__altura = ax
         
--- ClasesYobjetos/main.py
+++ ClasesYobjetos/main.py
@@ -4,17 +4,17 @@ from Armas import *
 
 #solicitar datos Spartan
 print("===== Datos de Heroe ========")
-nombreS= input("Escribe el nombre de tu Spartan")
-especieS= input("Escribe la especie de tu Spartan")
-alturaS= float(input("Escribe la alturatu Spartan"))
+nombreS= input("Escribe el nombre de tu Spartan: ")
+especieS= input("Escribe la especie de tu Spartan: ")
+alturaS= float(input("Escribe la alturatu Spartan: "))
 print("")
 
 
 #solicitar datos del Nemesis
 print("===== Datos del Villano ========")
-nombreN= input("Escribe el nombre del Nemesis")
-especieN= input("Escribe la especie del Nemesis")
-alturaN = float(input("Escribe la altura del Nemesis"))
+nombreN= input("Escribe el nombre del Nemesis: ")
+especieN= input("Escribe la especie del Nemesis: ")
+alturaN = float(input("Escribe la altura del Nemesis: "))
 print("")
 
 #creamos el objeto Spartan de la clase personaje       
@@ -25,16 +25,16 @@ Arma= Armas()
 
 #Usamos los atributos Spartan
 print("===== El objeto Spartan contiene ========")
-print(Spartan.nombre)
-print(Spartan.especie)
-print(Spartan.altura)
+print(Spartan.getNombre())
+print(Spartan.getEspecie())
+print(Spartan.getAltura())
 print("")
 
 #Usamos los atributos Nemesis
 print("===== El objeto Nemesis contiene ========")
-print(Nemesis.nombre)
-print(Nemesis.especie)
-print(Nemesis.altura)
+print(Nemesis.getNombre())
+print(Nemesis.getEspecie())
+print(Nemesis.getAltura())
 print("")
 
 
@@ -50,17 +50,34 @@ Nemesis.correr(True)
 Nemesis.lanzarGranada()
 print("")
 
+
 #Usamos los Metodos del Arma
 
 print("====== Selecciona arma del Spartan ======")
-Arma.seleccionarArma(Spartan.nombre)
+Arma.seleccionarArma(Spartan.getNombre())
 Arma.recargarArma(65)
 print("")
 
 print("====== Selecciona arma del Villano ======")
-Arma.seleccionarArma(Nemesis.nombre)
+Arma.seleccionarArma(Nemesis.getNombre())
 Arma.recargarArma(10)
 print("")
 
+#cambiamos atributos del objeto Nemesis
+
+print("====== Cambio de Villano ======")
+
+cambioN= input("Escribe el nombre del nuevo villano: ")
+Nemesis.setNombre(cambioN)
+cambioE= input("Escribe la especie del nuevo villano: ")
+Nemesis.setEspecie(cambioE)
+print("")
+
+print("====== Villano Nuevo ======")
+print(Nemesis.getNombre())
+print(Nemesis.getEspecie())
+print("")
+
+ 
 
 
